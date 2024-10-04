c44cc005331099392b3736cf7f14daa2989b137d
Kamran3325 <mkamran0528@gmail.com>
2024-10-04 05:48:57 -0700

    Add files via upload

---
+++ lession2/app.js
@@ -0,0 +1,27 @@
+//        1.Adını soruş          //
+// let ad = prompt("Adınızı yazın");
+// alert(`Salam ${ad}`);
+//-------------------------------//
+
+//        2. İki ədədi topla      //
+// let a = Number(prompt("Birinci ədəd"));
+// let b = Number(prompt("İkinci ədəd"));
+// alert(a+b);
+//-------------------------------//
+
+//     3. Kvadratın sahəsi        //
+// let teref = Number(prompt('Kvadratın tərəfinin uzunluğunu yazın'));
+// let sahe = teref * teref;
+// alert(`Kvadratın sahəsi: ${sahe}`);
+//-------------------------------//
+
+//     4.Doğum ilini hesablama    //
+// let yas =Number(prompt("yaşınız"));
+// alert(2024 - yas);
+//-------------------------------//
+
+//        5.Riyazi əməllər         //
+// let a = Number(prompt("Birinci ədəd"));
+// let b = Number(prompt("İkinci ədəd"));
+// alert(`Cəmi: ${a+b} Fərqi: ${a-b} Hasili: ${a*b} Qismti: ${a/b}`);
+//-------------------------------//
